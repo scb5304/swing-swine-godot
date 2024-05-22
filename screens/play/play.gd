1ff51b380ad050a73b7462bb052e063b6d8cd3ba
scb5304 <scb5304@gmail.com>
2024-05-22 15:31:11 -0400

    Physics seems like a waste

--- screens/play/play.gd
+++ screens/play/play.gd
@@ -9,4 +9,15 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$Piggy/Sprite2D.rotation += 1 * delta
+	$Piggy.rotation = 1 * $Piggy/Sprite2D.rotation
 	pass
+
+
+func _on_piggy_body_entered(body):
+	print("here")
+	pass # Replace with function body.
+
+
+func _on_piggy_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	print("here again")
+	pass # Replace with function body.
