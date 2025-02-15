91e5421d6867fb711db72cee070eb537ef4ef575
scb5304 <scb5304@gmail.com>
2025-02-15 15:49:05 -0500

    4 coins

--- screens/play/play.gd
+++ screens/play/play.gd
@@ -1,24 +1,45 @@
 extends Node2D
 
+var score = 0
+var speed = 1
+
+func onAnyCoinHit():
+	score += 1
+	speed += 0.08
+	return
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Coins/RightCoin.visible = true
+	$Coins/BottomCoin.visible = true
+	$Coins/LeftCoin.visible = true
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$Piggy.rotation += 0.02
+	$Piggy.rotation += speed * delta
 	pass
 
+func _on_right_coin_body_entered(body):
+	print("Right")
+	onAnyCoinHit()
+	$Coins/RightCoin.visible = false
+	$Coins/TopCoin.visible =true
 
+func _on_bottom_coin_body_entered(body):
+	print("Bottom")
+	onAnyCoinHit()
+	$Coins/BottomCoin.visible = false
+	$Coins/RightCoin.visible = true
 
-func _on_right_coin_coin_hit():
-	print("ouch")
-	# $RightCoin.visible = false
-	$RightCoin.collision_mask = 5
-	$RightCoin.set_deferred("disabled", true)
-	$RightCoin.set_deferred("freeze", true)
-	$RightCoin.set_freeze_mode(RigidBody2D.FREEZE_MODE_STATIC)
-	# Reset position?
+func _on_left_coin_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	print("Left")
+	onAnyCoinHit()
+	$Coins/LeftCoin.visible = false
+	$Coins/BottomCoin.visible = true
 
-	pass # Replace with function body.
+func _on_top_coin_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	print("Top")
+	onAnyCoinHit()
+	$Coins/TopCoin.visible = false
+	$Coins/LeftCoin.visible = true
