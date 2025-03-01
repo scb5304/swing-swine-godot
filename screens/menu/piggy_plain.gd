4480a80bc2940c9c1649c6414443ffad5eaf5a75
scb5304 <scb5304@gmail.com>
2025-03-01 13:36:51 -0500

    Clicking main menu piggy

--- screens/menu/piggy_plain.gd
+++ screens/menu/piggy_plain.gd
@@ -1,17 +1,19 @@
-extends Node2D
-#
-#var amplitude: float = deg_to_rad(5)  # Swing angle in radians (5 degrees)
-#var period: float = 5.0            # Time for a full oscillation (seconds)
-#var time_passed: float = 0.0
-#
-#func _process(delta: float) -> void:
-	#time_passed += delta
-	#rotation = amplitude * sin(2 * PI * time_passed / period)
+extends Area2D
 
 @export var max_angle: float = 7.0  # Maximum swing angle in degrees
 @export var swing_speed: float = 1.1  # Adjust speed of swinging
 
+@onready var piggy_sprite = $Sprite2D
+
 var time_elapsed: float = 0.0
+
+func _ready():
+	self.input_pickable = true
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventScreenTouch or event is InputEventMouseButton:
+		if event.pressed:
+			$OinkSound.play()
 
 func _process(delta):
 	time_elapsed += delta
