86e90aeb8008fb9f669c847f1e0f0752953610c5
scb5304 <scb5304@gmail.com>
2025-03-01 11:11:44 -0500

    Purchase confirmations and sounds

--- screens/market/piggy_market.gd
+++ screens/market/piggy_market.gd
@@ -4,6 +4,9 @@ const ShopItemScene = preload("res://screens/market/shop_item.tscn")
 
 @onready var items_container = $ScrollContainer/VBoxContainer
 
+var pending_purchase_item
+var pending_shop_item_scene
+
 func _ready():
 	_display_money_total()
 	_load_shop_items()
@@ -21,32 +24,43 @@ func _load_shop_items():
 		items_container.add_child(shop_item_scene)
 	
 func _on_item_selected(shop_item_scene, item):
-	print("Selected item: " + item["name"])
 	var owned: bool = item.get("owned", false)
+	var equipped: bool = item.get("equipped", false)
 	if (owned):
-		_equip_item(shop_item_scene, item)
+		if (!equipped):
+			_equip_item(shop_item_scene, item)
+		else:
+			_unequip_item(shop_item_scene, item)
 		_load_shop_items()
 	else:
-		_buy_item(shop_item_scene, item)
+		_confirm_buy_item(shop_item_scene, item)
 	
-func _buy_item(shop_item_scene, item):
+func _confirm_buy_item(shop_item_scene, item):
 	var price: int = item["price"]
 	if (price <= GameData.money_total):
-		print("I can afford this")
-		item["owned"] = true
-		GameData.money_total -= price
-		_display_money_total()
-		_equip_item(shop_item_scene, item)
-		_load_shop_items()
-		GameData.save_game()
-	else:
-		print("I can't do it")
+		$PurchaseDialog.dialog_text = "Purchase " + item.name + " for " + str(item.price) + " coins?"
+		self.pending_purchase_item = item
+		$PurchaseDialog.popup_centered()
+
+func _on_purchase_dialog_confirmed():
+	var item = pending_purchase_item
+	var shop_item_scene = pending_shop_item_scene
+	item["owned"] = true
+	GameData.money_total -= item["price"]
+	_display_money_total()
+	_equip_item(shop_item_scene, item)
+	_load_shop_items()
+	$PurchaseSound.play()
+	GameData.save_game()
 
 func _equip_item(shop_item_scene, item):
 	for game_data_item in GameData.items:
 		if game_data_item.slot == item.slot:
 			game_data_item["equipped"] = false
 	item["equipped"] = true
+	
+func _unequip_item(shop_item_scene, item):
+	item["equipped"] = false
 
 func _on_main_menu_button_pressed():
 	get_tree().change_scene_to_file("res://screens/menu/menu.tscn")
@@ -55,13 +69,10 @@ func _update_item(shop_item_scene, item):
 	shop_item_scene.get_node("Name").text = item["displayName"]
 	shop_item_scene.get_node("Price").text = str(item["price"])
 	shop_item_scene.get_node("Icon").texture = load("res://assets/images/accessories/" + item["name"] + ".png")
-	
-	if item.get("owned", false):	
+	shop_item_scene.get_node("OwnedText").visible = false
+	shop_item_scene.get_node("EquippedText").visible = false
+
+	if item.get("equipped", false):
+		shop_item_scene.get_node("EquippedText").visible = true
+	elif item.get("owned", false):
 		shop_item_scene.get_node("OwnedText").visible = true
-		if item.get("equipped", false):
-			print(item["name"] + " is equipped")
-			shop_item_scene.get_node("OwnedText").text = "Equipped"
-		else:
-			shop_item_scene.get_node("OwnedText").text = "Owned"
-	else:
-		shop_item_scene.get_node("OwnedText").visible = false
